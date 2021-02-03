8596682b5ede45c7a940c4a2072ffcc9abb83c59
Bob <bmarkoski@gmail.com>
2021-02-03 23:26:39 +0100

    index.js vas not saved

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import ScrollToTop from "./ScrollToTop";
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
